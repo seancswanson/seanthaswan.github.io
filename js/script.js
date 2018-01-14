98416ef616650729cfb4d67ac1c03f5cd51db7a2
seanthaswan <sean-swan@hotmail.com>
2018-01-14 15:21:29 -0800

    Added Carousel

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $( document ).ready(function(){
 
   $(".button-collapse").sideNav();
-  
+      $('.carousel').carousel({
+            dist:0,
+            shift:0,
+            padding:20,
+            indicators: true
+      });
+
+        
 });
