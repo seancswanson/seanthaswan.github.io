576f8c8011dc66284542b0f97b6ad1968cf61a0f
seanthaswan <sean-swan@hotmail.com>
2018-01-17 09:22:02 -0800

    Added new accordion, added portrait

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $( document ).ready(function(){
   $(".button-collapse").sideNav();
   $('.carousel.carousel-slider').carousel({
             dist:0,
-            shift: 1,
+            shift: 0,
             fullWidth: true
       });
   $('.next').click(function() {
@@ -13,28 +13,19 @@ $( document ).ready(function(){
     $('.carousel.carousel-slider').carousel('prev');
   });
         
-
- 
-
-
-$(".div--container__technologies > ul").on("mousedown touchstart", function(e) {
-
-    if (!$(this).hasClass("active")) {
-      console.log('clicked');
-    // hide any open menus and remove all other classes
-    $(".collapsible-body").slideUp(300);
-    $(".div--container__technologies > ul").removeClass("active");
-
-    // open our new menu and add the open class
-    $(this).next("div").slideDown(300);
-    $(this).addClass("active");
+  $('.toggle').click(function(e) {
+    e.preventDefault();
+  
+    var $this = $(this);
+  
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+    } else {
+        $this.parent().parent().find('li .inner').removeClass('show');
+        $this.parent().parent().find('li .inner').slideUp(350);
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
     }
-
-    else if ($(this).hasClass("active")) {
-    $(this).removeClass("active");
-    $(this).next("div").slideUp(300);
-    console.log("slideback");
-    }
-
-});
+  });
 });
