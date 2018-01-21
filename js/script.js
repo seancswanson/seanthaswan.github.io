830cccfded09ce2dbabcd05c36f65d23dca60c13
seanthaswan <sean-swan@hotmail.com>
2018-01-21 10:54:20 -0800

    Added contact me section and experimenting with touch events

--- js/script.js
+++ js/script.js
@@ -28,4 +28,20 @@ $( document ).ready(function(){
         $this.next().slideToggle(350);
     }
   });
+
+$('.toggle').touchstart(function(e) {
+    e.preventDefault();
+  
+    var $this = $(this);
+  
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+    } else {
+        $this.parent().parent().find('li .inner').removeClass('show');
+        $this.parent().parent().find('li .inner').slideUp(350);
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+    }
+  });
 });
