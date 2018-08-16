2f51891f0bf3ab6c6bd7e0c152af72bc65044f1d
Sean Swanson <sean-swan@hotmail.com>
2018-08-16 12:38:45 -0700

    Added chroma-chameleon.

--- js/script.js
+++ js/script.js
@@ -3,21 +3,22 @@
   window.swanson = {
 
     $textBox: $(document).find('.main-home-textbox'),
-    
+
     $textBoxAbout: $(document).find('.main-about-textbox'),
-    
+
     $firstMainParagraph: $(document).find('.main--paragraph__first'),
-    
+
     $secondMainParagraph: $(document).find('.main--paragraph__second'),
-    
+
     $portfolioContentArea: $(document).find('.main-content-container'),
-    
+
     projects: [{
       links: [
       '//seancswanson.com/chairman-of-the-board',
       '//mndfll-js.herokuapp.com/',
       '//use-hue.herokuapp.com',
       '//pigmentation-presumption.glitch.me/',
+      '//chroma-chameleon.glitch.me/',
       '//github.com/seanthaswan/express-pokedex'
     ],
       screenshots: [
@@ -25,6 +26,7 @@
         'assets/mndfll-sm.png',
         'assets/hue-sm.png',
         'assets/pigmentation-sm.png',
+        'assets/chroma-sm.png',
         'assets/poke-sm.png'
       ],
       alt: [
@@ -32,6 +34,7 @@
         'Mndf.ll',
         'Hue',
         'Pigmentation Presumption',
+        'Chroma Chameleon',
         'PokéDex'
       ],
       titles: [
@@ -39,6 +42,7 @@
         'Mndf.ll',
         'Hue',
         'Pigmentation Presumption',
+        'Chroma Chameleon',
         'PokéDex'
       ],
       descriptions: [
@@ -46,12 +50,14 @@
         'A full-stack daily journaling & goal tracking app featuring user auth and database utilization.',
         'A MERN-stack color detection utility for designers featuring cloud uploading & asynchronous operations.',
         'A front-end color guessing game to help sharpen your eye for color! Soon to add HEX code challenge for the elites.',
+        'A front-end painting app resembling the nostalgic simplicity of MSPaint... Soon to add full shape, importing, selection functionalities.',
         'A PEN-stack Pokèmon encyclopedia as a study of RESTful API architecture and artistic design.'
       ],
       badges: [
         ['JavaScript', 'jQuery', 'HTML', 'CSS'],
         ['JavaScript', 'Node', 'Express', 'PostgreSQL', 'HTML', 'CSS'],
         ['JavaScript', 'Node', 'React', 'Express', 'MongoDB', 'HTML', 'CSS'],
+        ['JavaScript', 'HTML', 'CSS'],
         ['JavaScript', 'HTML', 'CSS'],
         ['JavaScript', 'Node', 'Express', 'PostgreSQL', 'HTML', 'CSS']
       ]
@@ -70,41 +76,41 @@
         var card = document.createElement('div');
         card.classList = 'card';
         this.$portfolioContentArea.append(card);
-        
+
         var cardImageContainer = document.createElement('div');
         cardImageContainer.classList = 'card-image-container';
         card.append(cardImageContainer);
-        
+
         var cardImageLink = document.createElement('a');
         cardImageLink.classList = 'card-image-link';
         cardImageLink.setAttribute('href', projects.links[i]);
         cardImageLink.style.backgroundImage = "url('" + projects.screenshots[i] + "')";
         cardImageContainer.append(cardImageLink);
-        
+
         // var cardImage = document.createElement('img');
         // cardImage.classList = 'card';
         // cardImage.setAttribute('src', projects.screenshots[i]);
         // cardImage.setAttribute('alt', projects.alt[i]);
         // cardImageLink.append(cardImage);
-        
+
         var cardInfo = document.createElement('div');
         cardInfo.classList = 'card-info';
         card.append(cardInfo);
-        
+
         var cardTitle = document.createElement('p');
         cardTitle.classList = 'card-title';
         cardTitle.textContent = projects.titles[i];
         cardInfo.append(cardTitle);
-        
+
         var cardDescription = document.createElement('p');
         cardDescription.classList = 'card-description';
         cardDescription.textContent = projects.descriptions[i];
         cardInfo.append(cardDescription);
-        
+
         var cardTech = document.createElement('p');
         cardTech.classList = 'card-tech';
         cardInfo.append(cardTech);
-        
+
         for (var j = 0; j <= projects.badges[i].length - 1; j++) {
           var cardTechBadge = document.createElement('span');
           cardTechBadge.classList = 'card-tech-badge';
