36401ab6267e48489010b3b5d853f636892cef6a
Sean Swanson <sean-swan@hotmail.com>
2018-09-03 21:39:56 -0700

    Added artwork

--- js/script.js
+++ js/script.js
@@ -19,7 +19,12 @@
       '//use-hue.herokuapp.com',
       '//pigmentation-presumption.glitch.me/',
       '//chroma-chameleon.glitch.me/',
-      '//github.com/seanthaswan/express-pokedex'
+      '//github.com/seanthaswan/express-pokedex',
+      'assets/cabin-eularetta-pixel-lg.jpg',
+      'assets/hat-redd-pixel-lg.jpg',
+      'assets/redbag-lux-pixel-lg.jpg',
+      'assets/redhouse-elliasen-pixel-lg.jpg',
+      'assets/valley-johannes-pixel-lg.jpg'
     ],
       screenshots: [
         'assets/chairman-sm.png',
@@ -27,7 +32,12 @@
         'assets/hue-sm.png',
         'assets/pigmentation-sm.png',
         'assets/chroma-sm.png',
-        'assets/poke-sm.png'
+        'assets/poke-sm.png',
+        'assets/cabin-eularetta-pixel-sm.jpg',
+        'assets/hat-redd-pixel-sm.jpg',
+        'assets/redbag-lux-pixel-sm.jpg',
+        'assets/redhouse-elliasen-pixel-sm.jpg',
+        'assets/valley-johannes-pixel-sm.jpg'
       ],
       alt: [
         'Chairman of the Board',
@@ -35,7 +45,12 @@
         'Hue',
         'Pigmentation Presumption',
         'Chroma Chameleon',
-        'PokéDex'
+        'PokéDex',
+        'Quiet Cabin',
+        'Alpine Acquiesce',
+        'Summit Sublimity',
+        'Stiled Seculusion',
+        'Valley of Virtue'
       ],
       titles: [
         'Chairman of the Board',
@@ -43,7 +58,12 @@
         'Hue',
         'Pigmentation Presumption',
         'Chroma Chameleon',
-        'PokéDex'
+        'PokéDex',
+        'Quiet Cabin',
+        'Alpine Acquiesce',
+        'Summit Sublimity',
+        'Stilted Seculusion',
+        'Valley of Virtue'
       ],
       descriptions: [
         'A retro front-end corporate ladder simulator game. I reimagined a game that my father had programmed in BASIC in the 80\'s to bring it up to modern specs!',
@@ -51,7 +71,12 @@
         'A MERN-stack color detection utility for designers featuring cloud uploading & asynchronous operations.',
         'A front-end color guessing game to help sharpen your eye for color! Soon to add HEX code challenge for the elites.',
         'A front-end painting app resembling the nostalgic simplicity of MSPaint... Soon to add full shape, importing, selection functionalities.',
-        'A PEN-stack Pokèmon encyclopedia as a study of RESTful API architecture and artistic design.'
+        'A PEN-stack Pokèmon encyclopedia as a study of RESTful API architecture and artistic design.',
+        '8-Bit PNW Series: 1/5 - Adapted from eularetta (unsplash)',
+        '8-Bit PNW Series: 2/5 - Adapted from @reddangelo (unsplash)',
+        '8-Bit PNW Series: 3/5 - Adapted from @lux17 (unsplash)',
+        '8-Bit PNW Series: 4/5 - Adapted from @onesixtieth (unsplash)',
+        '8-Bit PNW Series: 5/5 - Adapted from @jlu (unsplash)'
       ],
       badges: [
         ['JavaScript', 'jQuery', 'HTML', 'CSS'],
@@ -59,7 +84,12 @@
         ['JavaScript', 'Node', 'React', 'Express', 'MongoDB', 'HTML', 'CSS'],
         ['JavaScript', 'HTML', 'CSS'],
         ['JavaScript', 'HTML', 'CSS'],
-        ['JavaScript', 'Node', 'Express', 'PostgreSQL', 'HTML', 'CSS']
+        ['JavaScript', 'Node', 'Express', 'PostgreSQL', 'HTML', 'CSS'],
+        ['Photoshop', 'Color Indexing', 'Level Manipulation', 'Pixel Art'],
+        ['Photoshop', 'Color Indexing', 'Level Manipulation', 'Pixel Art'],
+        ['Photoshop', 'Color Indexing', 'Level Manipulation', 'Pixel Art'],
+        ['Photoshop', 'Color Indexing', 'Level Manipulation', 'Pixel Art'],
+        ['Photoshop', 'Color Indexing', 'Level Manipulation', 'Pixel Art']
       ]
     }],
 
